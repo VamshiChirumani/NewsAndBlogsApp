325b0e92107e1600ccc983bb2489e759f15ea463
Cherumani Vamshidhar Reddy <vamshichirumani@gmail.com>
2025-03-21 16:51:28 -0500

    Nav bar is updated with user and categories

--- news-blogs-app/src/Components/News.jsx
+++ news-blogs-app/src/Components/News.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Weather from './Weather'
 import Calendar from './Calendar'
 import './News.css'
+import userImg from '../assets/images/DSC05373.JPG'
 
 const News = () => {
   return (
-    <div className='news'>
+    <div className="news">
       <header className="news-header">
         <h1 className="logo">News & Blogs</h1>
         <div className="search-bar">
-          <form> 
-            <input type='text' placeholder='search...' />
-            <button type='submit'>
+          <form>
+            <input type="text" placeholder="search..." />
+            <button type="submit">
               <i className="fa-solid fa-magnifying-glass"></i>
             </button>
           </form>
@@ -19,8 +20,45 @@ const News = () => {
       </header>
       <div className="news-content">
         <div className="nav-bar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+          <div className="user">
+            <img src={userImg} alt="User Image" />
+            <p>Vamshi's Blog</p>
+          </div>
+          <nav className="categories">
+            <h1 className="nav-headings">Categories</h1>
+            <div className="nav-links">
+              <a href="#" className="nav-link">
+                General
+              </a>
+              <a href="#" className="nav-link">
+                World
+              </a>
+              <a href="#" className="nav-link">
+                Business
+              </a>
+              <a href="#" className="nav-link">
+                Technology
+              </a>
+              <a href="#" className="nav-link">
+                Entertainment
+              </a>
+              <a href="#" className="nav-link">
+                Sports
+              </a>
+              <a href="#" className="nav-link">
+                Science
+              </a>
+              <a href="#" className="nav-link">
+                Health
+              </a>
+              <a href="#" className="nav-link">
+                Nation
+              </a>
+              <a href="#" className="nav-link">
+                Bookmarks <i className="fa-regular fa-bookmark"></i>
+              </a>
+            </div>
+          </nav>
         </div>
         <div className="news-section">
           <div className="headline">Headline</div>
@@ -28,14 +66,13 @@ const News = () => {
         </div>
         <div className="my-blogs">My Blogs</div>
         <div className="weather-calendar">
-          <Weather/>
-          <Calendar/>
+          <Weather />
+          <Calendar />
         </div>
-
       </div>
       <footer className="news-footer">Footer</footer>
     </div>
-  )
+  );
 }
 
 export default News
