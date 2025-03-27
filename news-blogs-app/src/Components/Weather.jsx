3bc6ebeae59e40115185bdb187493e6aec0e2852
Cherumani Vamshidhar Reddy <vamshichirumani@gmail.com>
2025-03-27 18:26:02 -0500

    Added functions to change icons and update the weather conditions

--- news-blogs-app/src/Components/Weather.jsx
+++ news-blogs-app/src/Components/Weather.jsx
@@ -1,25 +1,114 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Weather.css"
+import axios from 'axios'
+import math from 'react'
 
 const Weather = () => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const fetchDefaultLocation = async () => {
+      const defaultLocation = "Dallas";
+      const apikey = import.meta.env.VITE_WEATHER_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=${apikey}`;
+
+      const response = await axios.get(url);
+
+      setData(response.data);
+      console.log(data);
+    };
+    fetchDefaultLocation();
+  }, []);
+
+  const search = async () => {
+    const apikey = import.meta.env.VITE_WEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${apikey}`;
+
+    try {
+      const response = await axios.get(url);
+      if (response.data.cod !== 200) {
+        setData({ notfound: true });
+      } else {
+        setData(response.data);
+        setLocation("");
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        setData({ notFound: true });
+      } else {
+        console.error("An unexpected error occured", error);
+      }
+    }
+    console.log(data);
+  };
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter'){
+      search()
+    }
+  }
+
+  const getWeatherIcon = (weatherType) => {
+    switch (weatherType) {
+      case "Clear":
+        return <i className="bx bxs-sun"></i>;
+      case "Clouds":
+        return <i className="bx bxs-cloud"></i>;
+      case "Rain":
+        return <i className="bx bxs-cloud-rain"></i>;
+      case "Thunderstorm":
+        return <i className="bx bxs-cloud-lightening"></i>;
+      case "Snow":
+        return <i className="bx bxs-cloud-snow"></i>;
+      case "Haze":
+      case "Mist":
+        return <i className="bx bxs-cloud"></i>;
+      default:
+        return <i className="bx bxs-cloud"></i>;
+    }
+  };
+
   return (
-    <div className='weather' > 
-        <div className="search">
-          <div className="search-top">
-            <i className="fa-solid fa-location-dot">
-              <div className="location">Dallas</div>
-            </i>
-          </div>
-          <div className="search-location">
-            <input type="text" placeholder='Enter location' />
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </div>
+    <div className="weather">
+      <div className="search">
+        <div className="search-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="location">{data.name}</div>
         </div>
+        <div className="search-location">
+          <input
+            type="text"
+            placeholder="Enter location"
+            value={location}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+        </div>
+      </div>
+
+      {data.notFound ? (
+        <div> Not Found 😢</div>
+      ) : (
         <div className="weather-data">
-        <i className='bx bxs-sun'></i>
+          {data.weather &&
+            data.weather[0] &&
+            getWeatherIcon(data.weather[0].main)}
+          <div className="weather-type">
+            {data.weather ? data.weather[0].main : null}
+          </div>
+          <div className="temp">
+            {data.main ? `${Math.floor(data.main.temp)}⁰` : null}
+          </div>
         </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Weather
