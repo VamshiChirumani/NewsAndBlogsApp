dcc02c18f76d573d75b4fb83b1911ad9649e2c51
Cherumani Vamshidhar Reddy <vamshichirumani@gmail.com>
2025-03-27 13:50:12 -0500

    Added the required elements for the component

--- news-blogs-app/src/Components/Weather.jsx
+++ news-blogs-app/src/Components/Weather.jsx
@@ -4,7 +4,20 @@ import "./Weather.css"
 const Weather = () => {
   return (
     <div className='weather' > 
-        Weather
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot">
+              <div className="location">Dallas</div>
+            </i>
+          </div>
+          <div className="search-location">
+            <input type="text" placeholder='Enter location' />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+        <div className="weather-data">
+        <i className='bx bxs-sun'></i>
+        </div>
     </div>
   )
 }
