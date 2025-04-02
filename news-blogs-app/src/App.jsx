2259477e8c615e4dfe0cabc6390165deaa64900f
Cherumani Vamshidhar Reddy <vamshichirumani@gmail.com>
2025-04-02 17:59:35 -0500

    Form creating and submitting functionality is implemented

--- news-blogs-app/src/App.jsx
+++ news-blogs-app/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import News from './Components/News'
 import Blogs from './Components/Blogs'
 
 const App = () => {
+  const [showNews, setShowNews] = useState(true)
+  const [showBlogs, setShowBlogs] = useState(false)
+  const [blogs, setBlogs] =useState([])
+
+  const handleCreatedBlog = (newBlog) =>{
+    setBlogs((prevBlogs) => [...prevBlogs, newBlog])
+  }
+
+  const handleShowBlogs = () => {
+    setShowBlogs(true);
+    setShowNews(false);
+  };
+
+  const handleBackToNews = () => {
+    setShowBlogs(false);
+    setShowNews(true);
+  };
   return (
     <div className='container'>
       <div className='news-blogs-app'>
-        {/* <News/> */}
-        <Blogs />
+        {showNews && <News onShowBlogs={handleShowBlogs} blogs={blogs}/> }
+        {showBlogs && <Blogs onBack={handleBackToNews} onCreateBlog={handleCreatedBlog}/> }
       </div>
     </div>
   )
